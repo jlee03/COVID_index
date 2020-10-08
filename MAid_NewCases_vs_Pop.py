5b61ae381d2c11eca70ca9bdd44765e1635553a8
HuanlinDai <52108800+HuanlinDai@users.noreply.github.com>
2020-10-08 17:58:51 -0500

    Update MAid_NewCases_vs_Pop.py

--- MAid_NewCases_vs_Pop.py
+++ MAid_NewCases_vs_Pop.py
@@ -5,3 +5,8 @@ Created on Thu Oct  8 16:39:41 2020
 @author: daihu
 """
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+hold = pd.read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv",usecols=['population','new_cases','total_cases'])
